5b0de497240714168a9354d922f172facfadfe5f
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-07-17 06:15:35 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { SuperHeroes } from "./components/SuperHeroes.page";
+import { RQSuperHeroes } from "./components/RQSuperHeroes.page";
+import { HomePage } from "./components/Home.page";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to={"/super-heroes"}>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to={"/rq-super-heroes"}>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/super-heroes" element={<SuperHeroes />}></Route>
+            <Route path="/rq-super-heroes" element={<RQSuperHeroes />}></Route>
+            <Route path="/" element={<HomePage />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/components/SuperHeroes.page.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export const SuperHeroes = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheroes").then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
