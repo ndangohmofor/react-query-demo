8db6205a6e92b82b21cbb379706b451612fc3989
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-08-05 10:57:49 -0400

    Add the initial data to the useQuery call

---
+++ src/hooks/useDeviceProfileValidation.ts
@@ -0,0 +1,9 @@
+// import { useQueryClient } from "@tanstack/react-query";
+
+// const queryClient = useQueryClient();
+// const createPayload = async () => {
+//     let payload: any = {};
+//     try {
+//         const applicant: any = await queryClient.fetchQuery(applicant, () => null);
+//     }
+// }
